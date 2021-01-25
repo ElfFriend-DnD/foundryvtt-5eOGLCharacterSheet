93968b1c7f79ece43d853914c8ea3af6a636d5cb
akrigline <a.krigline@gmail.com>
2021-01-25 00:36:42 -0500

    clean up and remove some settings0

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,6 @@
 export const MODULE_ID = '5e-ogl-character-sheet';
 
 export enum MySettings {
-  limitActionsToCantrips = 'limit-actions-to-cantrips',
   showIconsOnInventoryList = 'show-icons-on-inventory-list',
   expandedLimited = 'expanded-limited',
 }
--- src/module/settings.ts
+++ src/module/settings.ts
@@ -4,15 +4,6 @@ export const registerSettings = function () {
   CONFIG[MODULE_ID] = { debug: false };
 
   // Register any custom module settings here
-  game.settings.register(MODULE_ID, MySettings.limitActionsToCantrips, {
-    name: 'OGL5E.limitActionsToCantrips',
-    default: false,
-    type: Boolean,
-    scope: 'client',
-    config: true,
-    hint: 'OGL5E.limitActionsToCantripsHint',
-  });
-
   game.settings.register(MODULE_ID, MySettings.showIconsOnInventoryList, {
     name: 'OGL5E.showIconsOnInventoryList',
     default: false,
